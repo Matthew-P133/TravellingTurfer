a16cd5a2de9cf6a06936302be18d9e43592cc6cc
Matthew-P133 <matthew.peters.work@gmail.com>
2022-07-15 17:33:00 +0100

    added first functional test

--- routing/tests.py
+++ routing/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
 
-# Create your tests here.
+import geckodriver_autoinstaller
+
+# install geckodriver if not installed and add it to path
+geckodriver_autoinstaller.install()
+
+class PageLoadTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def testPageLoads(self):
+        self.browser.get(self.live_server_url)
+        self.assertIn('TravellingTurfer', self.browser.title)
+
