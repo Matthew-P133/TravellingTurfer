d6347872939aa00ed6960287c14bc0608c94f463
Matthew-P133 <matthew.peters.work@gmail.com>
2022-07-22 12:13:11 +0100

    refactor

---
+++ routing/bruteForce.py
@@ -0,0 +1,60 @@
+from routing.models import Waypoints, Zone, Distance, Route
+
+def optimise(zones, distanceMatrix):
+
+    routeLength = len(zones)
+
+    # all possible routes
+    routes = []
+
+    # add first zone to route
+    route = [zones[0],]
+
+    # zones still to be visited
+    notInRoute = [zones[i] for i in range(1,routeLength)]
+
+    # brute force generate all routes
+    findAllRoutes(route, notInRoute, routes)
+
+    # find shortest
+    shortestDistance = 0
+    shortestRoute = []
+    
+    for route in routes:
+        distance = routeDistance(route)
+        if (distance < shortestDistance or shortestDistance == 0):
+            shortestDistance = distance
+            shortestRoute = route
+
+    # save shortest route to database
+    return shortestRoute
+    
+
+def routeDistance(route):
+
+    distance = 0
+    for i, zone in enumerate(route):
+        if i != len(route)-1:
+            start = Zone.objects.get(id=route[i])
+            end = Zone.objects.get(id=route[i+1])
+            distance += Distance.objects.get(zone_a=start, zone_b=end).distance
+
+    return distance
+        
+
+def findAllRoutes(route, notInRoute, routes):
+
+    # if all zones in route, go back to start and add route to routes
+    if len(notInRoute) == 0:
+        route.append(route[0])
+        routes.append(route)
+
+    # otherwise for each not in route zone add it to the route, make a new call to findAllRoutes
+    else:
+        for zone in notInRoute:
+            newRoute = route.copy()
+            newRoute.append(zone)
+            
+            newNotInRoute = notInRoute.copy()
+            newNotInRoute.remove(zone)
+            findAllRoutes(newRoute, newNotInRoute, routes)
--- routing/views.py
+++ routing/views.py
@@ -1,10 +1,11 @@
-from tracemalloc import start
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.urls import reverse
 import requests
 import json
 from routing.models import Waypoints, Zone, Distance, Route, createRoute
+import routing.bruteForce as bruteForce
+
 
 
 # redirect / to map page
@@ -55,9 +56,6 @@ def optimise(request):
     # populate distance matrix from database
     distanceMatrix = { zone:{} for zone in zones}
 
-    # toggle straight line / snap to map routing
-    straight = True
-
     for zoneA in zones:
         for zoneB in zones:
             start = Zone.objects.get(id=zoneA)
@@ -66,72 +64,13 @@ def optimise(request):
             distanceMatrix[zoneA].update({zoneB: distance})
 
     # calculate shortest route
-    response = bruteForceRoute(zones, distanceMatrix)
+    shortestRoute = bruteForce.optimise(zones, distanceMatrix)
+
+    # save it to the database
+    route = createRoute(shortestRoute)
 
     # returns route id of newly created route; javascript redirects user route page
-    return HttpResponse(response)
-
-
-def bruteForceRoute(zones, distanceMatrix):
-
-    routeLength = len(zones)
-
-    # all possible routes
-    routes = []
-
-    # add first zone to route
-    route = [zones[0],]
-
-    # zones still to be visited
-    notInRoute = [zones[i] for i in range(1,routeLength)]
-
-    # brute force generate all routes
-    findAllRoutes(route, notInRoute, routes)
-
-    # find shortest
-    shortestDistance = 0
-    shortestRoute = []
-    
-    for route in routes:
-        distance = routeDistance(route)
-        if (distance < shortestDistance or shortestDistance == 0):
-            shortestDistance = distance
-            shortestRoute = route
-
-    # save shortest route to database
-    route = createRoute(shortestRoute)    
-
-    return route.id
-    
-
-def routeDistance(route):
-
-    distance = 0
-    for i, zone in enumerate(route):
-        if i != len(route)-1:
-            start = Zone.objects.get(id=route[i])
-            end = Zone.objects.get(id=route[i+1])
-            distance += Distance.objects.get(zone_a=start, zone_b=end).distance
-
-    return distance
-        
-
-def findAllRoutes(route, notInRoute, routes):
-
-    # if all zones in route, go back to start and add route to routes
-    if len(notInRoute) == 0:
-        route.append(route[0])
-        routes.append(route)
-
-    # otherwise for each not in route zone add it to the route, make a new call to findAllRoutes
-    else:
-        for zone in notInRoute:
-            newRoute = route.copy()
-            newRoute.append(zone)
-
-            newNotInRoute = notInRoute.copy()
-            newNotInRoute.remove(zone)
-            findAllRoutes(newRoute, newNotInRoute, routes)
+    return HttpResponse(route.id)
 
 
 # generates geoJSON for display of a given route
